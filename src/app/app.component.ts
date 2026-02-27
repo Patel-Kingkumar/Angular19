f5ed69061e3a05db009454bc4beaeddc58269927
Patel King <kingpatel0409@gmail.com>
2026-02-27 22:06:42 +0530

    001  : Understand simaple signal concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-19';
+
+  count = signal(10);
+
+  constructor() {
+    // effect => A reactive function that runs automatically whenever the signals it reads change.
+    effect(() => {
+      console.log('count is ', this.count());
+    });
+  }
+
+  incr() {
+    // this.count.set(100);
+    this.count.update(c => c + 1);
+  }
+
+  decr() {
+    this.count.update(c => c - 1);
+  }
 }
